3a1cab3e7f3a640fe1fa73c6e0829cd301ca311c
Mamdouh El Nakeeb <mamdouh.el.nakeeb@gmail.com>
2018-03-12 05:18:16 +0200

    Discover Courses

---
+++ MyCourses - iOS/My Courses/Cells/CoursesDiscoverTVCell.swift
@@ -0,0 +1,27 @@
+//
+//  CoursesDiscoverTVCell.swift
+//  My Courses
+//
+//  Created by Mamdouh El Nakeeb on 3/12/18.
+//  Copyright © 2018 My Courses. All rights reserved.
+//
+
+import UIKit
+
+class CoursesDiscoverTVCell: UITableViewCell {
+
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var name: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
